07b90ff2dfeb0ecf991defbe83b151c0f957b924
GideonQ <114097172+GideonQ@users.noreply.github.com>
2022-10-05 22:38:06 -0400

    Add files via upload
    Conditions.py

---
+++ conditions.py
@@ -0,0 +1,16 @@
+#Gideon Quaye
+#conditions.py
+temp = a
+howMany = int(input("Please enter the current temperature."))
+if temp > 90:
+    print("Wear shorts")
+else > 70:
+    print("Short sleeves are fine")
+else > 50:
+    print("Wear a jacket")
+else > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay Inside")
+
+    
